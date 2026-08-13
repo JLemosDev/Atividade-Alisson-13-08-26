442db587d6d1c27ff71dfa1563b453d7c02f3204
João Vitor Lemos <63517229+JLemosDev@users.noreply.github.com>
2026-08-13 23:28:17 +0000

    add return

--- Atividade 1 Biblioteca/Autor.java
+++ Atividade 1 Biblioteca/Autor.java
@@ -4,7 +4,15 @@ public class Autor {
 
     public Autor(string nome, string nacionalidade ) {
         if (nome == null || nome.isblank()) throw new
-        i
+        illegalArgumentException("Nome do autor obrigadorio");
+        this.nome = nome;
+        this.nacionalidade = nacionalidade 
     }
+
+    public string getNome() { return nome; }
+    public string getNacionalidade() { return nacionalidade; }
+
+    @Override
+    public string tostring() { return nome; }
 
 }
