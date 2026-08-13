9c5205f20ab9fcd73d9c65f15f069086d60037eb
João Vitor Lemos <63517229+JLemosDev@users.noreply.github.com>
2026-08-13 23:17:01 +0000

    Inicio

---
+++ Atividade 1 Biblioteca/Autor.java
@@ -0,0 +1,9 @@
+public class Autor {
+    private final string nome;
+    private final string nacionalidade;
+
+    public Autor(string nome, string nacionalidade ) {
+        if (nome == null || nome.isblank()) throw new
+    }
+
+}
