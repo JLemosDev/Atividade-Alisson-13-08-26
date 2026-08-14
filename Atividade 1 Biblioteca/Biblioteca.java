5b82e9e29e9d34ef86c2e2deb2da33ef75990a68
João Vitor Lemos <63517229+JLemosDev@users.noreply.github.com>
2026-08-14 02:10:01 +0000

    add devolver

--- Atividade 1 Biblioteca/Biblioteca.java
+++ Atividade 1 Biblioteca/Biblioteca.java
@@ -2,5 +2,7 @@ public class Biblioteca {
     public Emprestimo emprestar(Livro livro, Usuario usuario) {
         return Emprestimo realizar(livro, usuario);
     }
-    
+    public void devolver(Emprestimo emprestimo) {
+        emprestimo devolver();
+    }
 }
