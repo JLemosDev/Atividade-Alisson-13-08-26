88582bc9e4e36812f3a01bf7e5d4cc213e1fb6e0
João Vitor Lemos <63517229+JLemosDev@users.noreply.github.com>
2026-08-20 16:06:47 -0300

    Atualizar o DescontoClientePremium.java

--- Atividade 4 descontos/DescontoClientePremium.java
+++ Atividade 4 descontos/DescontoClientePremium.java
@@ -1,6 +1,6 @@
 public class DescontoClientePremium implements EstrategiaDesconto {
     @Override
     public double calcular(Pedido pedido) {
-        return pedido.getValorTotal() * 0.10; // 10% de desconto
+        return pedido.getValorTotal() * 0.10;
     }
 }
