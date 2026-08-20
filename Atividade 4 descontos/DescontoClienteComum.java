38a3e6ecd53af802069f979ddc0b615f80a6208e
João Vitor Lemos <63517229+JLemosDev@users.noreply.github.com>
2026-08-20 16:07:06 -0300

    Atualizar o DescontoClienteComum.java

--- Atividade 4 descontos/DescontoClienteComum.java
+++ Atividade 4 descontos/DescontoClienteComum.java
@@ -1,6 +1,6 @@
 public class DescontoClienteComum implements EstrategiaDesconto {
     @Override
     public double calcular(Pedido pedido) {
-        return 0.0; // Cliente comum não tem desconto base
+        return 0.0;
     }
 }
