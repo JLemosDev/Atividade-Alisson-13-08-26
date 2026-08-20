a15d6784a8f31c289900c66d7df3cd0b1ca5d5eb
João Vitor Lemos <63517229+JLemosDev@users.noreply.github.com>
2026-08-20 16:06:21 -0300

    Atualizar o DescontoSazonal.java

--- Atividade 4 descontos/DescontoSazonal.java
+++ Atividade 4 descontos/DescontoSazonal.java
@@ -1,6 +1,6 @@
 public class DescontoSazonal implements EstrategiaDesconto {
     @Override
     public double calcular(Pedido pedido) {
-        return 50.0; // Desconto fixo de R$ 50
+        return 50.0;
     }
 }
